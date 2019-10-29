068bbae042de6ac6fcde4b1b0294ec80e870c96f
喻聪 <yucongcong@i-syp.com>
2019-10-29 16:33:54 +0800

    功能完善

---
+++ chapter-25-shiro-2/src/main/java/com/yucong/constants/SystemConsts.java
@@ -0,0 +1,15 @@
+package com.yucong.constants;
+
+/**
+ * 系统常量
+ * 
+ * */
+public interface SystemConsts {
+
+	 
+	/**
+     * 不需要权限验证的资源表达式
+     */
+    String[] NONE_PERMISSION_RES = {"/login"};
+	 
+}
